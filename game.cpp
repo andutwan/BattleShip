4363e8636e2caf825f43831ec3dcf6fd031da082
andutwan <andrewtran2638@gmail.com>
2023-02-04 00:08:01 -0800

    update game.cpp

--- game.cpp
+++ game.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+    Board humanBoard;
+    Board robotBoard;
+    Player human(humanBoard);
+    Computer robot(robotBoard);
+
+    playGame(human, robot);
+
+    return 0;
+}
